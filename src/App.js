1f1931dc4c8370eff3a1f1798901ca3f54798df9
Tim Bauer <timbauer@ymail.com>
2021-10-14 16:57:45 -0700

    tut complete up through 'adding more components'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import Backdrop from "./components/Backdrop";
+import Modal from "./components/Modal";
+import Todo from "./components/Todo";
 
 function App() {
-  return (<div>Hello!</div>);
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text='Learn React' />
+      <Todo text='Master React' />
+      <Todo text='Explore the full React Course' />
+      <Modal />
+      <Backdrop />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,16 @@
+function Todo(props) {
+    const deleteHandler = () => {
+        console.log('clicked here');
+        console.log(props.text);
+    }
+  return (
+    <div className="card">
+      <h2>{props.text}</h2>
+      <div className="actions">
+        <button className="btn" onClick={deleteHandler}>Delete</button>
+      </div>
+    </div>
+  );
+}
+
+export default Todo;
